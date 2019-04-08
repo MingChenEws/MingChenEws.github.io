ae37f968067acdac82d6a63e9c4e0c8b0c1743cc
Ming Chen <ming.chen@earlywarning.cm>
2019-04-08 14:44:30 -0500

    Add PoC Codes-201904008-1444

--- js/driver.js
+++ js/driver.js
@@ -201,20 +201,18 @@ function processRegisterFormLocal(e) {
 				newUser.keyHandle = binToStr(newCredentialInfo.rawId);
 				alert(">> newUser.keyHandle("+newUser.keyHandle.length+")::: " + newUser.keyHandle+"#####");
 
-				var MCbase64 = base64.encode(newCredentialInfo.rawId);
 				var MCbase64url = base64url.encode(newCredentialInfo.rawId);
-				var MSbase64encode = base64encode(newCredentialInfo.rawId);
-				var MSarrayString = arrayBufferToString(newCredentialInfo.rawId);
-				/*
-				alert("newCredentialInfo.id["+newCredentialInfo.id.length+"],"+ 
-				"MCbase64["+MCbase64.length+"], compare="+(newCredentialInfo.id===MCbase64));
 				alert("newCredentialInfo.id["+newCredentialInfo.id.length+"],"+ 
 				"MCbase64url["+MCbase64url.length+"], compare="+(newCredentialInfo.id===MCbase64url));
+				
+				var MSbase64encode = base64encode(newCredentialInfo.rawId);
 				alert("newCredentialInfo.id["+newCredentialInfo.id.length+"],"+ 
 				"MSbase64encode["+MSbase64encode.length+"], compare="+(newCredentialInfo.id===MSbase64encode));
+
+				var MSarrayString = arrayBufferToString(newCredentialInfo.rawId);
 				alert("newCredentialInfo.id["+newCredentialInfo.id.length+"],"+ 
 				"MSarrayString["+MSarrayString.length+"], compare="+(newCredentialInfo.id===MSarrayString));
-				*/
+
 			}
 			alert(">>4");
             accounts.push(newUser);
